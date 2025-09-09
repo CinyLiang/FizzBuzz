dcea80e128c3ca4a3953ef1966fcf445cb56bb25
CinyLiang <ciny.liang@mail.utoronto.ca>
2025-09-09 14:59:39 -0400

    Multiples generalized and tested

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,16 +2,20 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int n = 0;
-        for (int i = 1; i < 1000; i++) {
-            boolean divisibleby3 = i % 3 == 0;
-            boolean divisibleby5 = i % 5 == 0;
-            if (divisibleby3 && divisibleby5) {
-                n += 1;
-            } else if (divisibleby3 || divisibleby5) {
-                n += 1;
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            boolean divisiblebya = i % a == 0;
+            boolean divisiblebyb = i % b == 0;
+            if (divisiblebya && divisiblebyb) {
+                count += 1;
+            } else if (divisiblebya || divisiblebyb) {
+                count += 1;
             }
         }
-        System.out.println(n);
+        return count;
     }
 }
